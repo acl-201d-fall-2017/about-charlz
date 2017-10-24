9a727d68ab2dbe948c225a46f69015c0bc90ab0f
CharlyWelch <blwbiology@gmail.com>
2017-10-24 14:01:08 -0700

    scaffolding of js added

--- app.js
+++ app.js
@@ -1,2 +1,51 @@
 "use strict"
 
+const username = prompt('Hi there! Welcome! What\'s your name?')
+console.log('User name: ' + username);
+
+alert('Hello, ' + username + ' ! I\'m going to ask you a series of yes/no questions to see how well you know me.')
+
+const siblings = prompt ( )
+console.log('Siblings? Answer: ' + siblings)
+if(siblings.toLowerCase = 'yes' || siblings.toLowerCase = 'y') {
+    alert('Actually, ' + username + ', I\'m an only child and I was raised by a single mom. Next question!')
+}
+else if(siblings.toLowerCase = 'no' || siblings.toLowerCase = 'n') {
+    alert('Great job, ' + username '! I do have a few step-siblings but we don\'t know each other very well. Next question!')
+}
+
+const children = prompt ( )
+console.log('Children? Answer: ' + children)
+if(children.toLowerCase = 'yes' || children.toLowerCase = 'y') {
+    alert('')
+}
+else if(children.toLowerCase = 'no' || children.toLowerCase = 'n') {
+    alert('')
+}
+
+const crafts = prompt ( )
+console.log('Do I enjoy crafts? Answer: ' + crafts)
+if(crafts.toLowerCase = 'yes' || crafts.toLowerCase = 'y') {
+    alert('')
+}
+else if(crafts.toLowerCase = 'no' || crafts.toLowerCase = 'n') {
+    alert('')
+}
+
+const travel = prompt ( )
+console.log('Overseas travel? Answer: ' + travel)
+if(travel.toLowerCase = 'yes' || travel.toLowerCase = 'y') {
+    alert('')
+}
+else if(travel.toLowerCase = 'no' || travel.toLowerCase = 'n') {
+    alert('')
+}
+
+const food = prompt ( )
+console.log('Do I love southern food? Answer: ' + food)
+if(food.toLowerCase = 'yes' || food.toLowerCase = 'y') {
+    alert('')
+}
+else if(food.toLowerCase = 'no' || food.toLowerCase = 'n') {
+    alert('')
+}
